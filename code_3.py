1d7f639da5131e4b84a76e2b89823926078629da
reddydub1ty <139818835+reddydub1ty@users.noreply.github.com>
2023-11-02 11:27:27 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,39 @@
+if __name__ == "__main__":
+import random
+    return data
+
+    for item in data:
+        print(f"Random Number: {item}")
+
+def generate_random_data():
+
+    data = generate_random_data()
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+    main()
+if __name__ == "__main__":
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+
+
+def main():
+    return data
+    for item in data:
+        print(f"Random Number: {item}")
+
+import random
+    data = generate_random_data()    main()
+
+    data = generate_random_data()
+import random
+    for item in data:
+
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+
+        print(f"Random Number: {item}")
+def main():
+    return data
+def generate_random_data():
